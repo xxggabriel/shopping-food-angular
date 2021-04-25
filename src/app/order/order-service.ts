b1fcc4cf3c7d37e6c5a791bc8b424919ac15270a
Gabriel Oliveira <gabriel@umobi.com.br>
2021-04-24 21:27:05 -0300

    Create componente order

---
+++ src/app/order/order-service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs/Observable";
+import { Order } from "./order.model";
+
+@Injectable()
+
+export class OrderService {
+         
+    constructor(
+        private cartService: ShoppingCartService,
+        private http: Http
+    ){}
+
+    itemsValue(): number {
+        return this.cartService.total()
+    }
+
+    cartItems() : CartItem[] {
+        return this.cartService.items
+    }
+
+    increaseQty(item: CartItem){
+        this.cartService.increaseQty(item);
+    }
+
+    decreaseQty(item: CartItem){
+        this.cartService.decreaseQty(item);
+    }
+
+    remove(item: CartItem){
+        this.cartService.removeItem(item);
+    }
+
+    clear(){
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string>{
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, 
+            JSON.stringify(order), 
+            new RequestOptions({headers: headers}))
+            .map(response => response.json())
+            .map(order => order.id)
+    }
+}
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { RadioOption } from 'app/shared/radio/radio-option.component';
+import { OrderService } from './order-service';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  paymentOptions: RadioOption[] = [
+    {label:'Denheiro', value: 'MON'},
+    {label:'Cartão de Débito', value: 'DEB'},
+    {label:'Cartão de Refeição', value: 'REF'},
+  ]
+
+  orderForm: FormGroup
+  delivery: number = 8
+  
+  constructor(
+    private orderService: OrderService,
+    private _router: Router,
+    private _formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.orderForm = this._formBuilder.group({
+      name : this._formBuilder.control(''),
+      email: this._formBuilder.control(''),
+      emailConfirmation: this._formBuilder.control(''),
+      address: this._formBuilder.control(''),
+      number: this._formBuilder.control(''),
+      optionalAddress: this._formBuilder.control(''),
+      paymentOption: this._formBuilder.control(''),
+    });
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  itemsValue() : number {
+    return this.orderService.itemsValue()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
+  }
+
+  checkoutOrder(order: Order){
+    order.orderItems = this.cartItems().map((item: CartItem)=>  new OrderItem(
+      item.quantity,
+      item.menuItem.id
+    ))
+
+    this.orderService.checkOrder(order).subscribe((orderId)=>{
+      this.orderService.clear()
+      this._router.navigate(['/order-summary'])
+    })
   }
 
 }
