3bbb11b53b96f6a7c97baf4ae5b982ec4a645137
Gabriel Oliveira <gabriel@umobi.com.br>
2021-04-24 21:26:28 -0300

    Add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { OrderSummaryComponent } from "./order-summary/order-summary.component";
+import { OrderComponent } from "./order/order.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
@@ -16,5 +18,7 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent},
         ]},
+    {path: 'order', component : OrderComponent},
+    {path: 'order-summary', component : OrderSummaryComponent},
     {path: 'about', component : AboutComponent},
 ]
